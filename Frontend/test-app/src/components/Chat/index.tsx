5a2f743985fa24a86a89e1c31ec35a33bff5452e
sangit sigdel <sangit.sigdel@gmail.com>
2023-11-19 13:31:04 +0000

    chore: removed self user from chat list

--- Frontend/test-app/src/components/Chat/index.tsx
+++ Frontend/test-app/src/components/Chat/index.tsx
@@ -57,7 +57,11 @@ export const Chat = () => {
       socket.auth = { userName };
 
       socket.on("users", (users) => {
-        users.map((el: { userID: string; username: string }) => {
+        const updatedUser = users.filter(
+          (user: { userID: string }) => user.userID !== socket.id
+        );
+
+        updatedUser.map((el: { userID: string; username: string }) => {
           setChatUsers((prev) => {
             return [
               ...prev,
@@ -81,7 +85,7 @@ export const Chat = () => {
 
       socket.on(
         "user disconnected",
-        async (user: { username: string; userID: string }) => {
+        (user: { username: string; userID: string }) => {
           setChatUsers((prev) => {
             return prev.filter((prevUser) => prevUser.user !== user.username);
           });
