fe5996920d6f739267684879e95607c4228bcad9
Sangit Sigdel <53471276+SangitSigdel@users.noreply.github.com>
2023-11-19 21:58:17 +0000

    chore: split user and chat routers and controllers (#20)

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,3 +1,4 @@
+import chatRouter from "./routes/chatRouter";
 import cors from "cors";
 import express from "express";
 import userRouter from "./routes/userRouter";
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/chat", chatRouter);
 
 app.use((req, res, next) => {
   res.status(404).send({
