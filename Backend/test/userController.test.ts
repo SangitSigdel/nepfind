1c6b40c29cf9a36940d0ea4ccb1cab7f6e5246bf
Sangit Sigdel <53471276+SangitSigdel@users.noreply.github.com>
2023-11-21 14:58:56 +0000

    chore: added user controller tests (#23)

--- Backend/test/userController.test.ts
+++ Backend/test/userController.test.ts
@@ -8,19 +8,26 @@ beforeAll(async () => {
   await mongoose.connect("mongodb://localhost:27017/testdb");
 });
 
+beforeEach(async () => {
+  // Clear the User collection before each test
+  await UserModel.deleteMany({});
+});
+
 // Disconnect from the test database after running tests
 afterAll(async () => {
   await mongoose.connection.close();
 });
 
 describe("UserController", () => {
-  beforeEach(async () => {
-    // Clear the User collection before each test
-    await UserModel.deleteMany({});
-  });
-
   test("GET /api/v1/user should return an empty array when no users are present", async () => {
-    const response = await request(app).get("/api/v1/user/sangit");
+    const newUser = await request(app).post("/api/v1/user/signup").send({
+      user_id: "user1",
+      user_name: "user1",
+    });
+    const response2 = await request(app).get("/api/v1/user/user1");
+    expect(response2.status).toBe(200);
+
+    const response = await request(app).get("/api/v1/user/user2");
     expect(response.status).toBe(404);
   });
 
@@ -30,8 +37,11 @@ describe("UserController", () => {
       user_name: "sangit",
     });
     expect(newUser.status).toBe(200);
-  });
 
-  // Implement other test cases for CRUD operations
-  // headless cms
+    const newUser2 = await request(app).post("/api/v1/user/signup").send({
+      user_id: "sangit",
+      user_name: "sangit",
+    });
+    expect(newUser2.status).toBe(400);
+  });
 });
