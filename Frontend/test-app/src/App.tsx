6c3c867db6ad124d56b93b155fb2bd94fdb3922a
Sangit Sigdel <53471276+SangitSigdel@users.noreply.github.com>
2023-12-02 10:09:49 +0000

    SPS-20: manage color used all over the website using theme (#39)

--- Frontend/test-app/src/App.tsx
+++ Frontend/test-app/src/App.tsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Chat, Login } from "./components";
 
 import { Helmet } from "react-helmet";
+import { ThemeProvider } from "styled-components";
+import theme from "./utils/theme";
 import { title } from "./utils/constants";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Helmet>
-        <title>{title}</title>
-      </Helmet>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- Frontend/test-app/src/components/Chat/ChatUsers.tsx
+++ Frontend/test-app/src/components/Chat/ChatUsers.tsx
@@ -1,29 +1,73 @@
-import { CurrentChatWithType } from "./index";
-import { Dispatch } from "react";
-import styled from "styled-components";
+import { Box, Typography } from "@mui/material";
+import React, { Dispatch } from "react";
+import styled, { useTheme } from "styled-components";
 
-const SelectUserWrapper = styled.div`
-  position: fixed;
-  padding-right: 20px;
-  p {
-    font-size: 18px;
-    margin: 0.2em;
-  }
-  padding-top: 50px;
-  z-index: 1;
+import Cookies from "js-cookie";
+import { CurrentChatWithType } from ".";
 
-  background-color: #3f0e40;
-  color: #ffff;
-  padding-top: 20px;
-  height: calc(100vh - 20px);
-  padding-left: 2em;
-`;
+export interface UserData {
+  status: string;
+  data: Data;
+}
+export interface Data {
+  _id: string;
+  user_id: string;
+  user_name: string;
+  online: boolean;
+  messages?: MessagesEntity[] | null;
+  __v: number;
+}
+export interface MessagesEntity {
+  user_id: string;
+  chats?: ChatsEntity[] | null;
+  _id: string;
+}
+export interface ChatsEntity {
+  chat_id: number;
+  message: string;
+  messageByUser: boolean;
+  dateTime: string;
+  _id: string;
+}
 
 export type ChatUsersType = {
   user: string;
   status: string;
   userId: string;
 };
+
+const UserListWrapper = styled.div`
+  padding: 10px 20px;
+  margin-right: 5px;
+  margin-top: 5px;
+  border-bottom: 1px solid ${(props) => props.theme.palette.border.main};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.palette.primary.light};
+  }
+`;
+
+const CustomTypography = styled(Typography)`
+  color: ${(props) => props.theme.palette.bright.main};
+  margin: 0;
+  padding: 0;
+`;
+
+const UserDisplayHeader = styled.div`
+  text-align: center;
+  padding: 20px 0;
+  background: ${(props) => props.theme.palette.primary.light};
+  width: inherit;
+`;
+
+const StatusCircle = styled.div<{ online: boolean }>`
+  width: 10px;
+  height: 10px;
+  margin: 10px;
+  border-radius: 50%;
+  background-color: ${(props) => (props.online ? "green" : "red")};
+`;
 
 type ChatUsersProps = {
   users: ChatUsersType[];
@@ -33,21 +77,59 @@ type ChatUsersProps = {
   currentChatWith?: CurrentChatWithType;
 };
 
-export const ChatUsers = ({ users, setCurrentChatWith }: ChatUsersProps) => {
+export const NewChatScreen = ({
+  users,
+  setCurrentChatWith,
+}: ChatUsersProps) => {
+  const loggedInUser = Cookies.get("userName");
+  const theme = useTheme();
   return (
-    <SelectUserWrapper>
-      {users.map((el, index) => (
-        <div
-          key={index}
-          style={{ cursor: "pointer" }}
-          onClick={() =>
-            setCurrentChatWith({ username: el.user, userID: el.userId })
-          }
-        >
-          <p>{el.user}</p>
-          <p style={{ color: "#d1d1d1", marginBottom: "20px" }}>{el.status}</p>
-        </div>
-      ))}
-    </SelectUserWrapper>
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        overflowY: "auto",
+        height: "100vh",
+        minWidth: "12em",
+        overflowX: "hidden",
+      }}
+    >
+      <UserDisplayHeader>
+        <CustomTypography variant="h5">{loggedInUser}</CustomTypography>
+      </UserDisplayHeader>
+      <Box>
+        {users.map((el, index) => {
+          return (
+            <Box>
+              <UserListWrapper
+                onClick={() =>
+                  setCurrentChatWith({
+                    username: el.user,
+                    userID: el.userId,
+                  })
+                }
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <CustomTypography variant="h6">{el.user}</CustomTypography>
+                  <StatusCircle online={true} />
+                </div>
+                <CustomTypography
+                  variant="subtitle1"
+                  sx={{ color: theme.palette.bright.light }}
+                >
+                  {"Test message"}
+                  {/* {msg.chats[msg.chats.length - 1].message} */}
+                </CustomTypography>
+              </UserListWrapper>
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
   );
 };
