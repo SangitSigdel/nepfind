a4b24d0bf1286dfcaf0fae415ebe48dc0e80acae
Sangit Sigdel <53471276+SangitSigdel@users.noreply.github.com>
2023-12-17 21:39:58 +0000

    sps-51: create basic test for message input component (#58)

---
+++ Frontend/chat-app/src/components/Chat/chatScreen/component/MessageInput.test.tsx
@@ -0,0 +1,49 @@
+import "@testing-library/jest-dom/extend-expect";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import { MessageInput } from "./MessageInput";
+import { ThemeProvider } from "styled-components";
+
+const customTheme = {
+  palette: {
+    bright: {
+      main: "#ffff",
+    },
+  },
+};
+
+const message = "";
+const setMessage = jest.fn();
+const sendPrivateMessage = jest.fn();
+
+const renderComponenet = () => {
+  return render(
+    <ThemeProvider theme={customTheme}>
+      <MessageInput
+        message={message}
+        setMessage={setMessage}
+        sendPrivateMessage={sendPrivateMessage}
+      />
+    </ThemeProvider>
+  );
+};
+
+describe("MessageInput Component", () => {
+  test("should render message box and send icon", () => {
+    renderComponenet();
+    const messageInputElement = screen.getByRole("textbox");
+    const sendButton = screen.getByRole("button");
+
+    expect(messageInputElement).toBeInTheDocument();
+    expect(sendButton).toBeInTheDocument();
+  });
+
+  test("should setMessage state on user input", async () => {
+    renderComponenet();
+    const messageInputElement = screen.getByRole("textbox");
+    fireEvent.change(messageInputElement, { target: { value: "hello world" } });
+
+    expect(setMessage).toBeCalledWith("hello world");
+  });
+});
